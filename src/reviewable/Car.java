a961a1aada417651af856d68106d3e9a21e566eb
koehler1000 <koehler9@gmail.com>
2018-06-10 18:59:05 +0200

    added getters and setters

--- src/reviewable/Car.java
+++ src/reviewable/Car.java
@@ -7,4 +7,20 @@ public class Car {
 		this.brand = brand;
 		this.plate = plate;
 	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getPlate() {
+		return plate;
+	}
+
+	public void setPlate(String plate) {
+		this.plate = plate;
+	}
 }
